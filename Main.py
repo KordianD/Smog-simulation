e9cbebb441913c0cd91abb4f010ffc4e96123058
kordianD <kordiandrozd@gmail.com>
2018-05-12 18:33:41 +0200

    Adding initialization of cells

--- Main.py
+++ Main.py
@@ -4,6 +4,14 @@ from Cell import Cell
 from Point import Point
 from configuration import *
 
-stove_coordinates = [Point(randint(0, MAP_HEIGHT - 1), randint(0, MAP_WIDTH - 1)) for _ in range(NUMBER_OF_STOVES)]
 
-city_map = [[Cell() for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
+class Main:
+    def __init__(self):
+        self.city_map = [[Cell() for _ in range(MAP_WIDTH)] for _ in range(MAP_HEIGHT)]
+        self.stove_coordinates = [Point(randint(0, MAP_HEIGHT - 1), randint(0, MAP_WIDTH - 1))
+                                  for _ in range(NUMBER_OF_STOVES)]
+        self.__initialize_cells()
+
+    def __initialize_cells(self):
+        for stove in self.stove_coordinates:
+            self.city_map[stove.x][stove.y].change_level_of_smog_production(STOVE_SMOG_PRODUCTION)
